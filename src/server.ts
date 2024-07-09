c67f696d4dc67dc0af300bf0e0af9c31e93fb564
arthur <arthur.rios007@gmail.com>
2024-07-09 20:43:48 -0300

    Aula 2

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,11 @@ import cors from '@fastify/cors'
 import { createTrip } from './routes/create-trip'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { confirmTrip } from './routes/confirm-trip'
+import { confirmParticipants } from './routes/confirm-participant'
+import { createActivity } from './routes/create-activity'
+import { getActivities } from './routes/get-activities'
+import { createLink } from './routes/create-link'
+import { getLinks } from './routes/get-links'
 
 const app = fastify()
 
@@ -12,6 +17,11 @@ app.register(cors, {
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
